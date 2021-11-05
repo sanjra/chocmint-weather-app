37d7272eb13cfb9b56befcd2ef030a0916022166
sanjra <sanjrabezzi@gmail.com>
2021-11-05 18:40:09 +1100

    ADDED search function

--- src/app.js
+++ src/app.js
@@ -66,10 +66,21 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = `34ae1065362d42545661451bda2b8a1f`;
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = `34ae1065362d42545661451bda2b8a1f`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("Milan");
+
+//console.log(apiUrl);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
