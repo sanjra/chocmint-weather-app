9e7ed67d68a93ef08a8da698e7e8bd6bcf8b1faa
sanjra <sanjrabezzi@gmail.com>
2021-11-06 11:22:29 +1100

    FIX errors
    temp bug
    text dec fixed

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.top;
+  celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
