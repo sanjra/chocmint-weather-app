d7626fa4b30c86f3430b7ff018cba627057e46b8
sanjra <sanjrabezzi@gmail.com>
2021-11-11 18:26:59 +1100

    UPDATE api for forecast

--- src/app.js
+++ src/app.js
@@ -44,7 +44,8 @@ function formatDate(timestamp) {
   return `${hours}:${minutes} | ${day} ${dateno} ${month}, ${year}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -67,6 +68,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  //console.log(coordinates);
+  let apiKey = `34ae1065362d42545661451bda2b8a1f`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unite=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -92,6 +101,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -141,8 +152,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-//moving this later
-displayForecast();
-
 // search fx at the bottom
-search("Milan");
+search("Sydney");
